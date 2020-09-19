062be64c6ba9dc75b6fd54795212a567a3424bd3
Duangkamol Yolprasarn <duangkamol.y@kbtg.tech>
2020-09-19 15:44:38 +0700

    insert

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -8,22 +9,19 @@ import (
 	"os"
 
 	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
 )
 
 func main() {
 	fmt.Println("hello hometic : I'm Gopher!!")
-
 	r := mux.NewRouter()
 	r.HandleFunc("/pair-device", PairDeviceHandler).Methods(http.MethodPost)
-
 	addr := fmt.Sprintf("0.0.0.0:%s", os.Getenv("PORT"))
 	fmt.Println("addr:", addr)
-
 	server := http.Server{
 		Addr:    addr,
 		Handler: r,
 	}
-
 	log.Println("starting...")
 	log.Fatal(server.ListenAndServe())
 }
@@ -43,6 +41,15 @@ func PairDeviceHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer r.Body.Close()
 	fmt.Printf("pair: %#v\n", p)
-
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = db.Exec("INSERT INTO pairs VALUES ($1,$2);", p.DeviceID, p.UserID)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(err.Error())
+		return
+	}
 	w.Write([]byte(`{"status":"active"}`))
 }
